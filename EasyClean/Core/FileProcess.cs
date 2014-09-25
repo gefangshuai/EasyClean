67784ec09a2c6868e1ce3c8b7300b98964a33e63
葛方帅 <gefangshuai@163.com>
2014-09-25 13:50:17 +0800

    修改界面

---
+++ EasyClean/Core/FileProcess.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyClean.Core
+{
+    /// <summary>
+    /// 文件处理类
+    /// </summary>
+    class FileProcess
+    {
+        public void Delete(DirectoryInfo directory, String[] suffixs)
+        {
+            
+        }
+    }
+}
--- EasyClean/MainWindow.xaml.cs
+++ EasyClean/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Windows.Forms;
 
 namespace EasyClean
 {
@@ -23,6 +27,23 @@ namespace EasyClean
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ChooseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            var result = fbd.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                FileTextBox.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void ProcessButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var filePath = FileTextBox.Text;
+            var directory = new DirectoryInfo(filePath);
+//            directory.get
         }
     }
 }
